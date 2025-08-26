49ac697301a54ff6cc3add408474a0ae7ea691b5
Aishagojo <gojoaisha@gmail.com>
2025-08-25 22:54:46 -0700

    added relatiochsip

--- Introduction_to_Django/django-models/relationship_app/models.py
+++ Introduction_to_Django/django-models/relationship_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)  # optional field
 
     def __str__(self):
         return self.name
@@ -11,13 +12,15 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    publication_year = models.IntegerField(default=2000)  # default added ✅
 
     def __str__(self):
         return self.title
 
 
 class Library(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200, default="Unknown")  # default added ✅
     books = models.ManyToManyField(Book, related_name="libraries")
 
     def __str__(self):
