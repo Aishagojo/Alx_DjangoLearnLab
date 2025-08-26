fe8c6b16f9c55412b80a18e22a5a6f04e218e651
Aishagojo <gojoaisha@gmail.com>
2025-08-25 22:34:11 -0700

    testing

--- Introduction_to_Django/django-models/LibraryProject/settings.py
+++ Introduction_to_Django/django-models/LibraryProject/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
- 'relationship_app', ]
-
+'LibraryProject.relationship_app',]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
