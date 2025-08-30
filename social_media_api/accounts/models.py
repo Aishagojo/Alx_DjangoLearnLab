6de34363d570aaad3606d2ed04da8e6fe56c1f66
Aishagojo <gojoaisha@gmail.com>
2025-08-30 14:48:47 -0700

    post

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -2,8 +2,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True)
+    bio = models.TextField(blank=True, default="Hello!")
     profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following_users', blank=True)
-    following = models.ManyToManyField('self', symmetrical=False, related_name='followers_users', blank=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
+
+    def __str__(self):
+        return self.username
 
--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomAuthToken.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    
-    # Follow / Unfollow endpoints
     path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow'),
     path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow'),
 ]
