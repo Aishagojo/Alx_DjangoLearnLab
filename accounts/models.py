b257c6772f3e2b45507f5a25515c2a65ca2d6098
Aishagojo <gojoaisha@gmail.com>
2025-08-30 12:37:38 -0700

    Implement accounts app: custom user model, serializers, views, and URL patterns

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,12 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
-    profile_picture = models.CharField(max_length=255, blank=True, null=True)  # Changed from ImageField
-    followers = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='following')
-    
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    followers = models.ManyToManyField(
+        "self", symmetrical=False, related_name="following", blank=True
+    )
+
     def __str__(self):
         return self.username
+
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),  # Add this line
+    path('api/', include('accounts.urls')),
 ]
 
