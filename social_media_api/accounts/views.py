e3828a5c33ead29797467d79319db773650471d9
Aishagojo <gojoaisha@gmail.com>
2025-08-30 16:05:12 -0700

    plz work

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,21 +1,24 @@
 # accounts/views.py
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from .models import CustomUser
 
 @login_required
 def follow_user(request, user_id):
     user_to_follow = get_object_or_404(CustomUser, id=user_id)
-    if user_to_follow != request.user:
-        # Add the user to the current user's following list
-        request.user.following.add(user_to_follow)
-    return redirect('profile', user_id=user_id)
+    if user_to_follow == request.user:
+        return JsonResponse({'status': 'error', 'message': 'Cannot follow yourself'}, status=400)
+    
+    request.user.following.add(user_to_follow)
+    return JsonResponse({'status': 'success', 'message': f'You are now following {user_to_follow.username}'})
 
 @login_required
 def unfollow_user(request, user_id):
     user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
-    if user_to_unfollow != request.user:
-        # Remove the user from the current user's following list
-        request.user.following.remove(user_to_unfollow)
-    return redirect('profile', user_id=user_id)
+    if user_to_unfollow == request.user:
+        return JsonResponse({'status': 'error', 'message': 'Cannot unfollow yourself'}, status=400)
+    
+    request.user.following.remove(user_to_unfollow)
+    return JsonResponse({'status': 'success', 'message': f'You have unfollowed {user_to_unfollow.username}'})
 
