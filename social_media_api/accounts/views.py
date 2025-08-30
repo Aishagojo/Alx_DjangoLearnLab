6509f250ec4eb5f7015e15436adf69f7e6134ec2
Aishagojo <gojoaisha@gmail.com>
2025-08-30 15:12:12 -0700

    post

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,53 +1,21 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
-from .serializers import RegisterSerializer
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
-CustomUser = get_user_model()
-
-# Registration view
-class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = RegisterSerializer
-
-# Login view
-class CustomAuthToken(ObtainAuthToken):
-    pass
-
-# Profile view
-class ProfileView(generics.RetrieveAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-    serializer_class = RegisterSerializer
-
-    def get_object(self):
-        return self.request.user
-
-# Follow another user
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        try:
-            user_to_follow = CustomUser.objects.get(id=user_id)
-        except CustomUser.DoesNotExist:
-            return Response({"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        target_user = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.add(target_user)
+        return Response({'detail': f'You are now following {target_user.username}.'})
 
-        request.user.following.add(user_to_follow)
-        return Response({"detail": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
-
-# Unfollow a user
 class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        try:
-            user_to_unfollow = CustomUser.objects.get(id=user_id)
-        except CustomUser.DoesNotExist:
-            return Response({"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        request.user.following.remove(user_to_unfollow)
-        return Response({"detail": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
+        target_user = get_object_or_404(CustomUser, id=user_id)
+        request.user.following.remove(target_user)
+        return Response({'detail': f'You have unfollowed {target_user.username}.'})
 
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,33 +1,15 @@
 from rest_framework import viewsets, permissions
-from rest_framework.generics import ListAPIView
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
-# CRUD for Posts
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
+class FeedView(viewsets.ViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-# CRUD for Comments
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-# Feed: posts from users the current user follows
-class FeedView(ListAPIView):
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return Post.objects.filter(
-            author__in=self.request.user.following.all()
-        ).order_by('-created_at')
+    def list(self, request):
+        # Get posts from users the current user follows
+        following_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
 
