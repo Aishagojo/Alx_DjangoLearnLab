456b8b9834112abc050ce96dd82de72416a88aa4
Aishagojo <gojoaisha@gmail.com>
2025-09-06 02:46:03 -0700

    updatetask2

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,36 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Author has a one-to-many relationship with Book
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ("Admin", "Admin"),
+        ("Librarian", "Librarian"),
+        ("Member", "Member"),
+    ]
 
-    def __str__(self):
-        return self.name
-
-
-# Book has a ForeignKey to Author (many books by one author)
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
-
-    def __str__(self):
-        return self.title
-
-
-# Library has many-to-many relationship with Book
-class Library(models.Model):
-    name = models.CharField(max_length=150)
-    books = models.ManyToManyField(Book, related_name="libraries")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return self.name
+        return f"{self.user.username} - {self.role}"
 
 
-# Librarian has one-to-one relationship with Library
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
-
-    def __str__(self):
-        return self.name
+# Signal to automatically create or update UserProfile when a User is created
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        # You can set a default role here if needed, e.g., 'Member'
+        UserProfile.objects.create(user=instance, role="Member")
+    else:
+        instance.userprofile.save()
 
