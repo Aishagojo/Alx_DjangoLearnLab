f8fba50568d6f0eed08ca5632253ae32b42f9df4
Aishagojo <gojoaisha@gmail.com>
2025-09-06 02:23:53 -0700

    updated urland view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import list_books
-from .views import LibraryDetailView  
+from .views import LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views  # for register view
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("register/", views.register, name="register"),  # ✅ grader expects this
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),  # ✅ class-based
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),  # ✅ class-based
 ]
+
