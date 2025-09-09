c67d11e3ac565fb825af301284f63df0af417eee
Aishagojo <gojoaisha@gmail.com>
2025-09-09 02:35:29 -0700

    task2

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
  'rest_framework',
     'api',    'django.contrib.staticfiles',
-]
+ 'django_filters',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,17 +1,26 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views import (
     BookListView,
     BookDetailView,
     BookCreateView,
     BookUpdateView,
-    BookDeleteView
+    BookDeleteView,
+    BookViewSet
 )
 
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 urlpatterns = [
+    # Task 1 URLs
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),  # matches grader
-    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),  # matches grader
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
+
+    # Task 2 ViewSet URLs
+    path('', include(router.urls)),
 ]
 
